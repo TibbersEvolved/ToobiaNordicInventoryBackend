ad882d6034ed1b7095958f6c6fe479cd6cee33a6
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-03-19 08:53:08 +0100

    Added equipment controller with get and create

---
+++ inventory/src/main/java/toobia/com/inventory/controller/EquipmentController.java
@@ -0,0 +1,32 @@
+package toobia.com.inventory.controller;
+
+import org.springframework.web.bind.annotation.*;
+import toobia.com.inventory.controller.web.EquipmentDto;
+import toobia.com.inventory.controller.web.EquipmentListResponseDto;
+import toobia.com.inventory.model.Equipment;
+import toobia.com.inventory.service.EquipmentService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/equipment")
+public class EquipmentController {
+
+    private EquipmentService equipmentService;
+
+    public EquipmentController(EquipmentService equipmentService) {
+        this.equipmentService = equipmentService;
+    }
+
+    @GetMapping
+    public EquipmentListResponseDto getAllEquipment() {
+        return EquipmentListResponseDto.toEquipmentListResponseDto
+                (equipmentService.getAllEquipment());
+    }
+
+    @PostMapping
+    public EquipmentDto createEquipment(@RequestBody EquipmentDto equipmentDto) {
+        equipmentService.addEquipment(equipmentDto.equipmentName(), equipmentDto.equipmentId());
+        return equipmentDto;
+    }
+
+}
---
+++ inventory/src/main/java/toobia/com/inventory/controller/web/EquipmentDto.java
@@ -0,0 +1,4 @@
+package toobia.com.inventory.controller.web;
+
+public record EquipmentDto(String equipmentName, String equipmentId) {
+}
