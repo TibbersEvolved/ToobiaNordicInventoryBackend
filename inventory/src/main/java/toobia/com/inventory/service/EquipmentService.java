ebfe89d9a74029961a4c91d82fe6a58be6a5e174
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-03-19 08:36:39 +0100

    Added Equipment Repository and Service with methods to create and getAll

---
+++ inventory/src/main/java/toobia/com/inventory/service/EquipmentService.java
@@ -0,0 +1,30 @@
+package toobia.com.inventory.service;
+
+import org.springframework.stereotype.Service;
+import toobia.com.inventory.model.Equipment;
+import toobia.com.inventory.repository.EquipmentRepository;
+
+import java.util.List;
+
+@Service
+public class EquipmentService {
+
+    private EquipmentRepository equipmentRepository;
+
+    public EquipmentService(EquipmentRepository equipmentRepository) {
+        this.equipmentRepository = equipmentRepository;
+    }
+
+    public Equipment addEquipment(String equipmentName, String equipmentId) {
+        return equipmentRepository.save(new Equipment(equipmentName, equipmentId));
+    }
+
+    public List<Equipment> getAllEquipment() {
+        return equipmentRepository.findAll();
+    }
+
+    private Equipment getEquipment(String equipmentId) {
+        return equipmentRepository.findById(equipmentId).get();
+    }
+
+}
