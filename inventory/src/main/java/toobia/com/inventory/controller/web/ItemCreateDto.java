b1987bcfc2655743e20d3b934cfb9c74a9c73412
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-03-20 11:45:33 +0100

    Added Spring Web Validation dependency. Added min(0) to ItemCreateDto

--- inventory/src/main/java/toobia/com/inventory/controller/web/ItemCreateDto.java
+++ inventory/src/main/java/toobia/com/inventory/controller/web/ItemCreateDto.java
@@ -1,6 +1,8 @@
 package toobia.com.inventory.controller.web;
 
+import jakarta.validation.constraints.Min;
+
 import java.util.UUID;
 
-public record ItemCreateDto(UUID storageId, String equipmentId, UUID responsibleId, Integer amount) {
+public record ItemCreateDto(UUID storageId, String equipmentId, UUID responsibleId, @Min(0) Integer amount) {
 }
